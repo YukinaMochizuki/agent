b2bcc6c63258c1b6eb55e36ebf292ebb3fee80e1
yukina <1p41p4jejo@gmail.com>
2021-06-04 02:48:09 +0800

    Add log return while non zero exit code

--- src/main/java/tw/yukina/agent/controller/Agent.java
+++ src/main/java/tw/yukina/agent/controller/Agent.java
@@ -6,26 +6,55 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Pattern;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @RestController
 @Validated
 public class Agent {
+
+    private static final Object processLock = new Object();
+
     @GetMapping(path="/api/joplin/note/{uuid}")
     public String openNote(@PathVariable @Pattern(regexp = "^([a-zA-Z\\d]{8})$") String uuid){
 
-        new Thread(() -> {
-            try {
-                new ProcessBuilder("i3-msg", "[class=Joplin]", "focus").start();
-                new ProcessBuilder("xdotool", "key", "F6").start();
-                Thread.sleep(10);
-                new ProcessBuilder("xdotool", "key", "BackSpace").start();
-                new ProcessBuilder("xdotool", "type", uuid).start();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
+        StringBuilder log = new StringBuilder();
+        log.append("<pre>");
 
+        try {
+            command(log, "i3-msg", "[class=Joplin]", "focus");
+            command(log, "xdotool", "key", "F6");
+            command(log, "xdotool", "key", "BackSpace");
+            Thread.sleep(100);
+            command(log, "xdotool", "type", uuid);
+        } catch (Exception e) {
+                e.printStackTrace();
+                log.append("</pre>");
+                return log.toString();
+        }
+
+        log.append("</pre>");
         return "<script> window.close() </script>";
+    }
+
+    private void command(StringBuilder log, String ... args) throws Exception {
+        synchronized (processLock) {
+            log.append("Execute command: ");
+            log.append(Arrays.toString(args));
+            log.append(System.lineSeparator());
+            ProcessBuilder processBuilder = new ProcessBuilder(args);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ( (line = reader.readLine()) != null) {
+                log.append(line);
+                log.append(System.lineSeparator());
+            }
+
+            int exitCode = process.waitFor();
+            if(exitCode != 0)throw new Exception("command return non zero exit code " + exitCode);
+        }
     }
 }
